9a1460ea4c594b178f185ca1684f9d37f2d9042b
Oksana Levinskaya <qa.oksanaa@gmail.com>
2023-07-19 23:35:12 +0300

    junit

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -1,29 +1,47 @@
 package guru.qa;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class FirstTest {
 
-    static void beforeAll () {
-        Configuratio
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("beforeAll");
+        //Configuration.browser = "chrome";
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        // open("https://google.com");
+        System.out.println("  beforeEach");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("afterAll");
+    }
+
+    @AfterEach
+    void afterEach() {
+        // open("https://google.com");
+        System.out.println("  afterEach");
     }
 
     @Test
-    void firstTest () {
-        System.out.println("   firstTest");
+    void firstTest() {
+        System.out.println("      firstTest");
         Assertions.assertTrue(3 > 2);
     }
 
     @Test
-    void secondTest () {
-        System.out.println("   secondTest");
+    void secondTest() {
+        System.out.println("      secondTest");
         Assertions.assertTrue(3 > 2);
     }
 
     @Test
-    void thirdTest () {
-        System.out.println("   thirdTest");
+    void thirdTest() {
+        System.out.println("      thirdTest");
         Assertions.assertTrue(3 > 2);
     }
 }
