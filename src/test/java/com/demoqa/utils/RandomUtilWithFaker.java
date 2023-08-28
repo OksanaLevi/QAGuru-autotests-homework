973a86466785212e8aedad89a955fd24e88c16b6
OksanaLevi <105660473+OksanaLevi@users.noreply.github.com>
2023-08-28 13:21:06 +0300

    Update RandomUtilWithFaker.java
    small adjustment

--- src/test/java/com/demoqa/utils/RandomUtilWithFaker.java
+++ src/test/java/com/demoqa/utils/RandomUtilWithFaker.java
@@ -19,8 +19,6 @@ public class RandomUtilWithFaker {
         return sb.toString();
     }
 
-    Faker faker = new Faker(new Locale("ru"));
-
     public static
     String
             firstName = randomFirstName(),
